08f290ec1e615c77d382af98320f280a7f599388
alexandrendev <alxfn86@gmail.com>
2024-12-12 18:38:45 -0300

    First commit

---
+++ GeometricEfficiencyTool/src/main/java/com/lafis/GeometricEfficiencyTool/GeometricEfficiencyToolApplication.java
@@ -0,0 +1,13 @@
+package com.lafis.GeometricEfficiencyTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometricEfficiencyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometricEfficiencyToolApplication.class, args);
+	}
+
+}
---
+++ GeometricEfficiencyTool/src/test/java/com/lafis/GeometricEfficiencyTool/GeometricEfficiencyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafis.GeometricEfficiencyTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometricEfficiencyToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
