22619bbe25e2a0a23b3c7843034a2c803ac7c639
alexandrendev <alxfn86@gmail.com>
2024-12-27 02:20:38 -0300

    feat: exception

---
+++ src/main/java/com/lafis/GeometricEfficiencyTool/SimulationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lafis.GeometricEfficiencyTool;
+
+public class SimulationNotFoundException extends RuntimeException {
+    public SimulationNotFoundException(String message) {
+        super(message);
+    }
+}
