02e564066924b40297c20cc0e6dd63f4ec547686
alexandrendev <alxfn86@gmail.com>
2025-01-20 21:51:40 -0300

    implemmenting source methods

--- src/main/java/com/lafis/GeometricEfficiencyTool/database/domain/source/CuboidSource.java
+++ src/main/java/com/lafis/GeometricEfficiencyTool/database/domain/source/CuboidSource.java
@@ -2,19 +2,29 @@ package com.lafis.GeometricEfficiencyTool.database.domain.source;
 
 import com.lafis.GeometricEfficiencyTool.database.domain.simulation.Coordinate;
 
+import java.util.Random;
+
 public class CuboidSource extends Source {
     private double height;
     private double width;
-    private double depht;
+    private double depth;
 
-    public Coordinate randomizeEmitionPoint(Double height) {
-        return null;
+    public Coordinate randomizeEmitionPoint(Double bottomHeight) {
+        Random random = new Random();
+
+        double x = (random.nextDouble() * 2 - 1) * width / 2;
+
+        double y = (random.nextDouble() * 2 - 1) * depth / 2;
+
+        double z = bottomHeight + random.nextDouble() * height;
+
+        return new Coordinate(x, y, z);
     }
 
-    public CuboidSource(double height, double width, double depht) {
+    public CuboidSource(double height, double width, double depth) {
         this.height = height;
         this.width = width;
-        this.depht = depht;
+        this.depth = depth;
     }
 
     public double getHeight() {
@@ -31,5 +41,13 @@ public class CuboidSource extends Source {
 
     public void setWidth(double width) {
         this.width = width;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
     }
 }
--- src/main/java/com/lafis/GeometricEfficiencyTool/database/domain/source/SphericalSource.java
+++ src/main/java/com/lafis/GeometricEfficiencyTool/database/domain/source/SphericalSource.java
@@ -2,13 +2,26 @@ package com.lafis.GeometricEfficiencyTool.database.domain.source;
 
 import com.lafis.GeometricEfficiencyTool.database.domain.simulation.Coordinate;
 
+import java.util.Random;
+
 
 public class SphericalSource extends Source{
     private double radius;
 
     @Override
-    public Coordinate randomizeEmitionPoint(Double height) {
-        return null;
+    public Coordinate randomizeEmitionPoint(Double bottomHeight) {
+        Random r = new Random();
+        double theta = r.nextDouble() * 2 * Math.PI;
+        double phi = r.nextDouble() * Math.PI;
+        double radialDistance = Math.cbrt(r.nextDouble()) * radius;
+
+        double x = radialDistance * Math.sin(phi) * Math.cos(theta);
+        double y = radialDistance * Math.sin(phi) * Math.sin(theta);
+        double z = radialDistance * Math.cos(phi);
+
+        z = bottomHeight + z;
+
+        return new Coordinate(x, y, z);
     }
 
     public SphericalSource(double radius) {
